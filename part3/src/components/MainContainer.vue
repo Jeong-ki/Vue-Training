ed21ca48eb79e7deb799de9bc67b6b70f1c82e95
Jeong-ki <7863z@naver.com>
2022-05-26 00:15:02 +0900

    part3 sns, use axios

---
+++ part3/src/components/MainContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <MainPost v-for="(data, i) in snsData" :key="i" :snsData="data" />
+  </div>
+</template>
+
+<script>
+import MainPost from "./MainPost.vue";
+
+export default {
+  components: {
+    MainPost,
+  },
+  props: {
+    snsData: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
